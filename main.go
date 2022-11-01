ed934e1ba835865ff5eaf3c52f4f9d232983aa2c
Hasan H. SAHIN <hsnsh@outlook.com>
2022-11-01 23:00:58 +0300

    first commit

--- main.go
+++ main.go
@@ -1,10 +1,31 @@
-//package clause
+// package clause
 package main
 
 //import statements
 import "fmt"
 
-//main code-block
+// main code-block
 func main() {
-	fmt.Println("Hello, 世界")
+
+	// assign and set decoupled syntax
+	// var name, surname string = "Hasan", "SAHIN"
+
+	//Assign and set couple syntax
+	name, surname := "Hasan", "SAHIN"
+
+	// assign and set syntax
+	// var age int = 40
+	age := 40
+
+	//only set syntax
+	age = 38
+
+	fmt.Println("Hello, ", name, surname, ". You are ", age, ".")
+
+	// var isMarried bool = true
+	isMarried := true
+
+	if isMarried {
+		fmt.Println("You get married")
+	}
 }
